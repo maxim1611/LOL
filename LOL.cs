0a15167f0c4ff4c72bd7e2bf5c1da37682dd1b5f
maxim1611 <119848931+maxim1611@users.noreply.github.com>
2022-12-05 16:16:08 +0700

    LOLIIII

---
+++ LOL.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LOL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(1234); 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(1234);
+        
+    }
+}
